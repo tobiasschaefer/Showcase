76aabe778886a8f1b460ec5e06a493582e3c3e6c
Marcel Sailer <d4rkq@gmx.de>
2018-06-16 10:44:39 +0200

    ECM-308: Extend database entity with status field(Enum)

--- backend/src/main/java/org/educama/shipment/model/Shipment.java
+++ backend/src/main/java/org/educama/shipment/model/Shipment.java
@@ -1,17 +1,11 @@
 package org.educama.shipment.model;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import org.educama.customer.model.Customer;
 import org.educama.enums.ClientType;
+import org.educama.enums.Status;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
@@ -48,4 +42,7 @@ public class Shipment extends AbstractPersistable<Long> {
 
     @Embedded
     public Flight shipmentFlight;
+
+    @Enumerated(EnumType.STRING)
+    public Status statusEnum;
 }
---
+++ backend/src/main/resources/db/migration/common/V01_011__add_status_to_shipment.sql
@@ -0,0 +1,1 @@
+alter table shipment add column status_enum varchar(255);
